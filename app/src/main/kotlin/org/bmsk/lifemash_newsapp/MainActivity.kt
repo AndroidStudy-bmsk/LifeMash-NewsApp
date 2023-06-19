43a9ee2313558a359c4344dd5ee85566dae2ed20
bmsk <vv99911@gmail.com>
2023-06-19 18:05:00 +0900

    Fix: 바인딩하는 중 오류 코드 개선, 더블 번 탭 종료

--- app/src/main/kotlin/org/bmsk/lifemash_newsapp/MainActivity.kt
+++ app/src/main/kotlin/org/bmsk/lifemash_newsapp/MainActivity.kt
@@ -1,21 +1,60 @@
 package org.bmsk.lifemash_newsapp
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.databinding.DataBindingUtil
 import dagger.hilt.android.AndroidEntryPoint
 import org.bmsk.lifemash_newsapp.databinding.ActivityMainBinding
+import kotlin.system.exitProcess
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val viewModel: MainViewModel by viewModels()
 
+    private var backPressedTime = 0L
+    private val exitAppWhenBackButtonPressedTwiceCallback =
+        createExitAppWhenBackButtonPressedTwiceCallback()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater).apply {
-            setContentView(root)
-            lifecycleOwner = this@MainActivity
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        onBackPressedDispatcher.addCallback(exitAppWhenBackButtonPressedTwiceCallback)
+    }
+
+    private fun createExitAppWhenBackButtonPressedTwiceCallback() =
+        object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (backButtonPressedTwiceWithinFinishInterval()) {
+                    exitApp()
+                } else {
+                    backPressedTime = System.currentTimeMillis()
+                    Toast.makeText(
+                        this@MainActivity,
+                        getString(R.string.guide_double_tab_exit),
+                        Toast.LENGTH_SHORT
+                    ).show();
+                }
+            }
+
+            private fun backButtonPressedTwiceWithinFinishInterval(): Boolean {
+                val currentTime = System.currentTimeMillis()
+                val timeSinceLastBackButtonPress = currentTime - backPressedTime
+                return timeSinceLastBackButtonPress in 0..FINISH_INTERVAL_TIME
+            }
+
+            private fun exitApp() {
+                ActivityCompat.finishAffinity(this@MainActivity)
+                exitProcess(0)
+            }
         }
+
+    companion object {
+        private const val FINISH_INTERVAL_TIME = 2000L
     }
 }
